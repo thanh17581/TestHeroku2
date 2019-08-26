2bdb6fe0850f5957202931124a8f5d8e497d0fc6
thanh17581 <thanhktgch17581@fpt.edu.vn>
2019-08-27 04:35:21 +0700

    a

--- connect.php
+++ connect.php
@@ -1,5 +1,11 @@
-<?php
-$conn = pg_connect("host=ec2-174-129-27-158.compute-1.amazonaws.com port=5432 dbname=d692tlp6mgc3mc 
-user=qmiwpgmtylvvnq password=63251db4218016ad466a25a36fe6b2c76742cb871fa11da4a1051b7ebe4e3a71") 
-or die("Connect Unsuccessful");
+<?php	
+    $db = parse_url(getenv("DATABASE_URL"));
+    $pdo = new PDO("pgsql:" . sprintf(
+        "host=%s;port=%s;user=%s;password=%s;dbname=%s",
+        $db["host"],
+        $db["port"],
+        $db["user"],
+        $db["pass"],
+    ltrim($db["path"], "/")
+    ));  
 ?>
---
+++ dbconnector.php
@@ -0,0 +1,3 @@
+<?php
+$conn = pg_connect("host=ec2-23-21-160-38.compute-1.amazonaws.com port = 5432 dbname= dbjfoa0gfe9evt user= jzidaowoheuhua password= fc1db7716f804b61c3182a5ff5bdeb1a34fee534a953a3d3369af51ca2b42d69") or die("unstable connection")
+  ?>
