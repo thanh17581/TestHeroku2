3c5abe2e3cb597753de4b39cc22e1dbebc4761c0
thanh17581 <thanhktgch17581@fpt.edu.vn>
2019-08-27 15:45:11 +0700

    a

--- home.php
+++ home.php
@@ -1,4 +1,26 @@
-
+<?php 
+require_once './dbconnector.php';  
+if(isset($_POST["username"]) && isset($_POST["pass"]))
+{
+	$user = $_POST["username"];
+	$pass = $_POST["pass"];
+	$sql ="SELECT * FROM account WHERE username = '$user' AND pass= '$pass'";
+	$rows = pg_query($sql); 
+	if(pg_num_rows($rows)==1) { ?>
+		<script>
+            alert("Login successfully!!");
+            window.location.href = "/home.php"; 
+        </script>
+    <?php
+    } else { 
+        ?> 
+            <script>
+                alert("Wrong Username/Password");
+                window.location.href = "/index.php"; 
+            </script>
+        <?php }
+}
+?>
 <!DOCTYPE html>
 <html>
 <head>
--- process.php
+++ process.php
@@ -4,28 +4,6 @@
 	<title></title>
 </head>
 <body>
-<?php 
-require_once './dbconnector.php';  
-if(isset($_POST["username"]) && isset($_POST["pass"]))
-{
-	$user = $_POST["username"];
-	$pass = $_POST["pass"];
-	$sql ="SELECT * FROM account WHERE username = '$user' AND pass= '$pass'";
-	$rows = pg_query($sql); 
-	if(pg_num_rows($rows)==1) { ?>
-		<script>
-            alert("Login successfully!!");
-            window.location.href = "/home.php"; 
-        </script>
-    <?php
-    } else { 
-        ?> 
-            <script>
-                alert("Wrong Username/Password");
-                window.location.href = "/index.php"; 
-            </script>
-        <?php }
-}
-?>
+
 </body>
 </html>
