0a4a5624b354e0c038b83d9e5c7b5429f2944b59
thanh17581 <thanhktgch17581@fpt.edu.vn>
2019-08-27 17:27:23 +0700

    Update home.php

--- home.php
+++ home.php
@@ -59,36 +59,49 @@ if(isset($_POST["username"]) && isset($_POST["pass"]))
 </style>
 </head>
 <body>
-<div class="head1">Product</div>
-<table>
-	<tr>
-		<th class="Bz">ID</th> 
-		<th class="Bz">Image</th>
-		<th class="Bz">Name</th>
-		<th class="Bz">Price($)</th>
-		<th class="Bz">Detail</th>
-	</tr>
-	<?php
-            require_once 'connect.php';
-            foreach ($resultSet as $row) {
+<<div>
+        <h1>Managing Product</h1>
+        <table>
+            <tr>
+                <th class="tit">ID</th>
+                <th class="tit">Name</th>
+                <th class="tit">Price ($)</th>
+                <th class="tit">Description</th>
+                <th class="tit">Editing</th>
+            </tr>
+
+            <?php
+            require_once './database.php';
+            $sql = "SELECT * FROM product";
+            $stmt = $pdo->prepare($sql);
+            foreach ($pdo->query($sql) as $row) {
             ?>
-	<tr>
-		<th class="Bz"><?= $row['productid']?></th> 
-		<th class="Bz"><?= $row['image']?></th> 
-		<th class="Bz"><?= $row['name']?></th>
-		<th class="Bz"><?= $row['price']?></th>
-		<th class="Bz"><?= $row['detail']?></th>
-	</tr>
-	<tr>
-		<form action='/delete.php' method="POST">
+                <tr>
+                    <td class="info"><?php echo $row['productid']?></td> 
+                    <td class="info"><?php echo $row['proname']?></td> 
+                    <td class="info"><?php echo $row['price']?></td> 
+                    <td class="info"><?php echo $row['descrip']?></td> 
+                    <td class="info">
+                        <form action='/delete.php' method="POST">
                             <input type='hidden' name='productid' value='<?php echo $row['productid']?>'>
                             <input class="edit-btn" type='submit' value='Delete'>
                         </form> <br>
-	</tr>
-<?php
-}
-?>
-</table>
-<button><a href="/add.php">Add</a></button>
+
+                        <form action="/update.php" method="POST">
+                            <input type='hidden' name='productid' value='<?php echo $row['productid']?>'>
+                            <input type='hidden' name='name' value='<?php echo $row['proname']?>'>
+                            <input type='hidden' name='price' value='<?php echo $row['price']?>'>
+                            <input type='hidden' name='descrip' value='<?php echo $row['descrip']?>'>
+                            <input class="edit-btn" type='submit' value='Update'>
+                        </form>
+                    </td>
+                </tr>
+            <?php
+            }
+            ?> 
+        </table>
+        <button><a href="/add.php">Add More</a></button>
+        <br><br>
+    </div>
 </body>
 </html>
