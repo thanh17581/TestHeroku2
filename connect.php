52c50bc24f3c0bb90737cb4a3de58cd86928e4df
thanh17581 <thanhktgch17581@fpt.edu.vn>
2019-08-28 10:15:05 +0700

    a

--- connect.php
+++ connect.php
@@ -1,6 +1,6 @@
 <?php
   if (empty(getenv("DATABASE_URL"))){
-      $pdo = new PDO('pgsql:host=localhost;port=5432;dbname=postgres', 'postgres', 'Candice');
+      $pdo = new PDO('pgsql:host=ec2-23-21-160-38.compute-1.amazonaws.com;port=5432;dbname=dbjfoa0gfe9evt', 'jzidaowoheuhua', 'fc1db7716f804b61c3182a5ff5bdeb1a34fee534a953a3d3369af51ca2b42d69');
   }  else {
     $db = parse_url(getenv("DATABASE_URL"));
     $pdo = new PDO("pgsql:" . sprintf(
