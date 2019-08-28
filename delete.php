664f79c1d37c434eff7c2b8f71d63dd611c78033
thanh17581 <thanhktgch17581@fpt.edu.vn>
2019-08-28 11:20:18 +0700

    Update delete.php

--- delete.php
+++ delete.php
@@ -1,7 +1,7 @@
 <?php
-require("connect.php");
+require("dbconnector.php");
 $id = $_POST['productid'];
 $sql = "DELETE FROM product WHERE productid = '$id'";
 pg_query($conn,$sql); 
-header("Location: /mhome.php");
+header("Location: /home.php");
 ?>
